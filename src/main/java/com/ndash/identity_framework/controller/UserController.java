6f6f815a8f6946bd41cab1cb7dce97fe8bae9b3c
Alwin Tom <hialwin94@gmail.com>
2025-08-16 19:52:06 +0200

    test commit

--- src/main/java/com/ndash/identity_framework/controller/UserController.java
+++ src/main/java/com/ndash/identity_framework/controller/UserController.java
@@ -16,6 +16,7 @@ import org.springframework.http.HttpStatus;
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
+
     private final UserService userService;
 
     public UserController(UserService userService) {
