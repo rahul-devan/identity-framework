daefe271fd2afde96c64b62f889b6b1d282e7714
Rahul Devan <inform.rahuldevan@gmail.com>
2025-11-08 20:58:23 +0530

    MInor fix in security

--- src/main/java/com/ndash/identity_framework/config/SecurityConfig.java
+++ src/main/java/com/ndash/identity_framework/config/SecurityConfig.java
@@ -3,9 +3,12 @@ package com.ndash.identity_framework.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
@@ -23,13 +26,18 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable()) // disable CSRF for REST APIs
+                .cors(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers(
                                 "/api/applications/**",
+
                                 "/api/roles/**",
                                 "/api/user/**"
-                        ).authenticated()
+                        ).
+                        authenticated()
                         .anyRequest().permitAll()
                 )
                 .oauth2ResourceServer(resourceServer ->
