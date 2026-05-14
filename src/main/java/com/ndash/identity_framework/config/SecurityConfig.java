46b075b6f3f919c8e468a7f598663ce7039a3a26
Rahul Devan <inform.rahuldevan@gmail.com>
2026-05-14 08:30:19 +0530

    enable auth

--- src/main/java/com/ndash/identity_framework/config/SecurityConfig.java
+++ src/main/java/com/ndash/identity_framework/config/SecurityConfig.java
@@ -30,7 +30,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .cors(Customizer.withDefaults())
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
